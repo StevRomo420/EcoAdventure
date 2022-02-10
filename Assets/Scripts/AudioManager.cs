7491272750d96d83426656117269251da9e3eacc
StevRomo420 <esteban5420978@hotmail.com>
2022-02-10 14:21:39 -0800

    Se integra el AudioManager a traves de 2 Scripts para poder gestionar los audios en el juego

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Audios[] audios;
+    public static AudioManager sharedInstance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sharedInstance = this;
+        foreach (Audios audioClip in audios)
+        {
+            audioClip.recurso = gameObject.AddComponent<AudioSource>();
+            audioClip.recurso.clip = audioClip.clip;
+
+            audioClip.recurso.volume = audioClip.volumen;
+            audioClip.recurso.pitch = audioClip.pitch;
+            audioClip.recurso.loop = audioClip.loop;
+        }
+    }
+
+    public void PlayAudio(string nombre)
+    {
+        Audios s = Array.Find(audios, audio => audio.nombre == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Aundio" + name + "no encontrado");
+            return;
+        }
+
+        s.recurso.Play();
+    }
+}
