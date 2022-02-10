b397509e8de0fa658ee96d3e81b507994c2582b9
StevRomo420 <esteban5420978@hotmail.com>
2022-02-09 16:36:38 -0800

    Se configuran las animaciones de los personajes en base a si se realizan las acciones correctas

--- Assets/Scripts/EcoBotController.cs
+++ Assets/Scripts/EcoBotController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EcoBotController : MonoBehaviour
 {
+
+    Jugador p2;
+
+    Animator animator;
+    //Convencion de constantes en c
+    const string PUNTO = "punto";
+    const string NO_PUNTO = "noPunto";
+    const string RETORNO = "retorno";
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject prueba = GameObject.Find("Jugador");
+        p2 = prueba.GetComponent<Jugador>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool(PUNTO, p2.ObtenerPunto());
+        animator.SetBool(NO_PUNTO, p2.PerderPunto());
+        animator.SetBool(RETORNO, true);
+
     }
+
+
 }
--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -7,14 +7,19 @@ public class Jugador : MonoBehaviour
 {
     public int vida=0;
     public int puntos=0;
- 
-  
+
+
+
+
+    public bool punto=false;
+    public bool noPunto=false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-       
 
+        
       
     }
 
@@ -28,12 +33,18 @@ public class Jugador : MonoBehaviour
     public void Ganar(int puntosObtenidos)
     {
         this.puntos += puntosObtenidos;
+        this.punto = true;
+        this.noPunto = false;
+        
     }
 
     public void Perder(int puntosObtenidos)
     {
         this.puntos -= puntosObtenidos;
         this.vida = vida - 1;
+        this.noPunto = true;
+        this.punto = false;
+       
     }
 
     public int totalDePuntos()
@@ -44,6 +55,16 @@ public class Jugador : MonoBehaviour
     public int vidaRestante()
     {
         return vida;
+    }
+
+    public bool ObtenerPunto()
+    {
+        return punto;
+    }
+
+    public bool PerderPunto()
+    {
+        return noPunto;
     }
 
 
