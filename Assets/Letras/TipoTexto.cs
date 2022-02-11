890ec0b24f1bc2d60c7d03a0c53761adc802963a
Aldahir Chaves Mora <ALDAHIR.CHAVES@ucr.ac.cr>
2022-02-11 13:32:54 -0600

    Se agrega la funcionalidad de tipo de fuente

---
+++ Assets/Letras/TipoTexto.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipoTexto : MonoBehaviour
+{
+
+    public string valorTexto;
+    public Text elementoTexto; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
