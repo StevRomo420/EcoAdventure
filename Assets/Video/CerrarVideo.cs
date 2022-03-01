96b70ba20fcc8f02a60a3212d9937492124c8ade
Erick Vasquez <erickmurillo0807@gmail.com>
2022-03-01 16:35:18 -0600

    se agrega el video de la historia al juego

---
+++ Assets/Video/CerrarVideo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CerrarVideo : MonoBehaviour
+{
+    public VideoPlayer video;
+    
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void Star() 
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckOver(VideoPlayer videoPlayer)
+    {
+        SceneManager.LoadScene("MenuPricipal");
+        gameObject.SetActive(false);
+    }
+}
